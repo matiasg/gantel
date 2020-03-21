3cfdf026902e6b50943241f1b03d03cb2f98b7a4
Matías Graña <matias.alejo@gmail.com>
2020-03-21 19:12:10 -0300

    simplify Task

--- Gantt.hs
+++ Gantt.hs
@@ -1,7 +1,6 @@
 module Gantt (
       Condition
     , Task(..)
-    , realEnd
     , taskWithStartAndSecondsDuration
     , aTimePoint
     ) where
@@ -10,7 +9,7 @@ import qualified Data.Time.Calendar as Calendar
 import qualified Data.Time.Clock as Clock
 import qualified Data.Fixed as Fixed
 import Control.Exception (assert)
-import Data.Maybe (catMaybes, mapMaybe)
+import Data.Maybe (catMaybes, mapMaybe, fromJust)
 
 aTimePoint :: Integer -> Int -> Int -> Integer -> Integer -> Integer -> Clock.UTCTime
 aTimePoint y m d hh mm ss = Clock.UTCTime (Calendar.fromGregorian y m d) (Clock.secondsToDiffTime $ hh * 3600 + mm * 60 + ss)
@@ -25,7 +24,7 @@ anInterval f1 f2 = (f1, f2)
 data Condition = RightAfter Task | At Clock.UTCTime deriving (Show, Eq)
 
 conditionStarts :: Condition -> Maybe Clock.UTCTime
-conditionStarts (RightAfter task) = start task
+conditionStarts (RightAfter task) = end task
 conditionStarts (At t) = Just t
 
 taskDependence :: Condition -> Maybe Task
@@ -34,33 +33,27 @@ taskDependence (At _) = Nothing
 
 -- Tasks
 data Task = Task { name :: String
-                 , start :: Maybe Clock.UTCTime
-                 , end :: Maybe Clock.UTCTime
+                 , duration :: Clock.NominalDiffTime
                  , startConditions :: [Condition]
                  } deriving (Show, Eq)
 
 taskWithStartAndSecondsDuration :: String -> Clock.UTCTime -> Integer -> [Condition] -> Task
-taskWithStartAndSecondsDuration name start duration startConditions = Task name (Just start) (Just dur) startConditions
-    where dur = Clock.addUTCTime (Clock.secondsToNominalDiffTime $ fromInteger duration) start
+taskWithStartAndSecondsDuration name start duration startConditions = Task name dur ((At start):startConditions)
+    where dur = (Clock.secondsToNominalDiffTime $ fromInteger duration)
 
-realEnd :: Task -> Clock.UTCTime
-realEnd (Task _ _ (Just e) _) = e
+start :: Task -> Maybe Clock.UTCTime
+start (Task _ _ cs) = maximum $ map conditionStarts cs
 
-
-taskDuration :: Task -> Maybe Clock.NominalDiffTime
-taskDuration (Task _ Nothing _ _) = Nothing
-taskDuration (Task _ _ Nothing _) = Nothing
-taskDuration (Task _ (Just s) (Just e) _) = Just $ Clock.diffUTCTime e s
-
-
-recomputeStart :: Task -> Task
-recomputeStart t@(Task _ _ _ []) = t
-recomputeStart (Task n s e cs) = Task n max_start e cs
-        where max_start = maximum $ s:(map conditionStarts cs)
+end :: Task -> Maybe Clock.UTCTime
+end t@(Task _ d cs) =
+    let strt = start t in
+        case strt of
+          Just beg -> Just $ Clock.addUTCTime d beg
+          Nothing -> Nothing
 
 
 taskDependencies :: Task -> [Task]
-taskDependencies (Task _ _ _ cs) = mapMaybe taskDependence cs
+taskDependencies (Task _ _ cs) = mapMaybe taskDependence cs
 
 
 type Project = [Task]
@@ -71,16 +64,17 @@ type Project = [Task]
 -- Tests
 f1 = aTimePoint 2020 2 29 18 5 23
 f2 = aTimePoint 2020 3 1 1 5 23
+dur = Clock.diffUTCTime f2 f1
 
-t1 = Task "task 1" (Just f1) (Just f2) []
-t2 = Task "task 2" Nothing Nothing [RightAfter t1]
+t1 = Task "task 1" dur [At f1]
+t2 = Task "task 2" 0 [RightAfter t1]
 
 t3 = taskWithStartAndSecondsDuration "task 3" f1 (3 * 60 * 60) [At f2]
 
-t4 = Task "task 4" Nothing Nothing [RightAfter t1, RightAfter t2, At f1, RightAfter t3]
+t4 = Task "task 4"  (Clock.secondsToNominalDiffTime 60 * 60) [RightAfter t1, RightAfter t2, At f1, RightAfter t3]
 
 main = do
-    print $ assert (taskDuration t3 == Just (3 * 60 * 60)) ("test 1 passed")
-    print $ assert (start (recomputeStart t3) == Just f2) ("test 2 passed")
+    print $ assert (duration t3 == Clock.secondsToNominalDiffTime (3 * 60 * 60)) ("test 1 passed")
+    print $ assert (start t3 == Just f2) ("test 2 passed")
     print $ assert (taskDependencies t4 == [t1, t2, t3]) ("test 3 passed")
-    print $ assert (start (recomputeStart (Task "nothing" Nothing Nothing [RightAfter t2])) == Nothing) ("test 4 passed")
+    print $ assert (start (Task "nothing" 0 [RightAfter t2]) == (Just f2)) ("test 4 passed")
