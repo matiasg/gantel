86e650dcca0f5fa8824d11dadb5069de8b57d68f
Matías Graña <matias.alejo@gmail.com>
2020-03-21 11:56:19 -0300

    taskDependencies

--- Gantt.hs
+++ Gantt.hs
@@ -10,6 +10,7 @@ import qualified Data.Time.Calendar as Calendar
 import qualified Data.Time.Clock as Clock
 import qualified Data.Fixed as Fixed
 import Control.Exception (assert)
+import Data.Maybe (catMaybes, mapMaybe)
 
 aTimePoint :: Integer -> Int -> Int -> Integer -> Integer -> Integer -> Clock.UTCTime
 aTimePoint y m d hh mm ss = Clock.UTCTime (Calendar.fromGregorian y m d) (Clock.secondsToDiffTime $ hh * 3600 + mm * 60 + ss)
@@ -54,8 +55,18 @@ taskDuration (Task _ (Just s) (Just e) _) = Just $ Clock.diffUTCTime e s
 
 recomputeStart :: Task -> Task
 recomputeStart t@(Task _ _ _ []) = t
-recomputeStart (Task n s e ds) = Task n max_start e ds
-        where max_start = maximum $ s:(map conditionStarts ds)
+recomputeStart (Task n s e cs) = Task n max_start e cs
+        where max_start = maximum $ s:(map conditionStarts cs)
+
+
+taskDependencies :: Task -> [Task]
+taskDependencies (Task _ _ _ cs) = mapMaybe taskDependence cs
+
+
+type Project = [Task]
+
+
+
 
 -- Tests
 f1 = aTimePoint 2020 2 29 18 5 23
@@ -66,6 +77,10 @@ t2 = Task "task 2" Nothing Nothing [RightAfter t1]
 
 t3 = taskWithStartAndSecondsDuration "task 3" f1 (3 * 60 * 60) [At f2]
 
+t4 = Task "task 4" Nothing Nothing [RightAfter t1, RightAfter t2, At f1, RightAfter t3]
+
 main = do
     print $ assert (taskDuration t3 == Just (3 * 60 * 60)) ("test 1 passed")
     print $ assert (start (recomputeStart t3) == Just f2) ("test 2 passed")
+    print $ assert (taskDependencies t4 == [t1, t2, t3]) ("test 3 passed")
+    print $ assert (start (recomputeStart (Task "nothing" Nothing Nothing [RightAfter t2])) == Nothing) ("test 4 passed")
