7191ad7e1f97ac8fdc59faea4c22a2f4d3f387cc
Matías Graña <matias.alejo@gmail.com>
2020-03-24 20:12:51 -0300

    remove example.hs

--- example.hs
+++
@@ -1,8 +0,0 @@
-import qualified Gantt
-
-main = do
-    let launch = Gantt.aTimePoint 2020 3 26 1 51 10
-    let leop = Gantt.Task "LEOP" (60 * 60 * 10) [Gantt.At launch]
-    let bacp = Gantt.Task "BACP" (60 * 60 * 24 * 7) [Gantt.RightAfter leop]
-    let bcpcp = Gantt.Task "BCPCP" (60 * 60 * 24 * 35) [Gantt.RightAfter bacp]
-    print $ Gantt.end bcpcp
